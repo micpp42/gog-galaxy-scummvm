c806fca2999b0e035c5cd55be3f2a95a9b15836c
micpp42 <michael.possingham@gmail.com>
2020-05-02 16:11:14 +1000

    Initial commit

---
+++ plugin.py
@@ -0,0 +1,64 @@
+import sys, os, configparser, subprocess
+from galaxy.api.plugin import Plugin, create_and_run_plugin
+from galaxy.api.consts import Platform, LocalGameState
+from galaxy.api.types import Authentication, Game, LicenseInfo, LicenseType, LocalGame
+
+SCUMMVM_INSTALL_PATH = 'C:\\Program Files\\ScummVM\\scummvm.exe'
+SCUMMVM_INI_PATH = os.path.join(os.getenv('appdata'), 'ScummVM\\scummvm.ini')
+
+
+class PluginExample(Plugin):
+
+
+
+
+    def __init__(self, reader, writer, token):
+        super().__init__(
+            Platform.Newegg,  # choose platform from available list
+            "0.1",  # version
+            reader,
+            writer,
+            token
+        )
+
+
+    def read_scummvm_ini_file(self):
+        inifile = configparser.ConfigParser()
+        inifile.read(SCUMMVM_INI_PATH)
+        games = []
+        for gameid in inifile.sections():
+            if gameid not in ['scummvm','cloud']:
+                games.append(Game(gameid,inifile[gameid]['description'].split(' (')[0],None, LicenseInfo(LicenseType.SinglePurchase)))
+
+        return games
+
+
+
+
+
+    async def authenticate(self, stored_credentials=None):
+        return Authentication('scummvm', 'ScummVM')
+
+
+    async def get_owned_games(self):
+        games = self.read_scummvm_ini_file()
+        return games
+
+    async def get_local_games(self):
+        games = self.read_scummvm_ini_file()
+        local_games = []
+        for game in games:
+            local_games.append(LocalGame(game.game_id, LocalGameState.Installed))
+        return local_games
+
+
+    async def launch_game(self, game_id):
+        return subprocess.Popen([SCUMMVM_INSTALL_PATH,game_id])
+
+
+def main():
+    create_and_run_plugin(PluginExample, sys.argv)
+
+# run plugin event loop
+if __name__ == "__main__":
+    main()
